0a3a6d2c289ab63022792b6fdc670f6d3c418180
SocratesWong <socrateswong@yahoo.com>
2015-11-03 11:25:15 -0500

    Upload

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+     static class Roll {
+        public static List<Person> allmembers = new List<Person>();
+        
+        public static String[] Depart = { "COMM", "D&I", "GR", "PRO", "SPA", "ENG", "AD" };
+        public static List<Person>[] Departmembers = new List<Person>[Depart.Length];
+        public static void gensList() {
+            for (int x = 0; x < Departmembers.Length;x++ )
+            {
+                Departmembers[x] = new List<Person>();
+            }
+            foreach (Person x in allmembers) {
+                for(int y=0; y<Depart.Length;y++) { 
+                    if (x.department.Equals(Depart[y]))
+                    {
+                        Departmembers[y].Add(x);
+                    }
+                }
+            }
+        }
+        public static Department[] depart = new Department[Depart.Length];
+        public static void start() {
+
+            Console.Out.Write("start");
+            depart = new Department[Depart.Length + 1];
+            Console.Out.Write(0);
+            depart[0] = new Department("Communications");
+            Console.Out.Write(1);
+            depart[1] = new Department("Diversity &amp; Inclusion");
+            Console.Out.Write(2);
+            depart[2] = new Department("Governmental Relations");
+            Console.Out.Write(3);
+            depart[3] = new Department("Programming");
+            Console.Out.Write(4);
+            depart[4] = new Department("Strategic Planning &amp; Assessment");
+            Console.Out.Write(5);
+            depart[5] = new Department("Student Organization Engagement");
+            Console.Out.Write(6);
+            depart[6] = new Department("Associate Directors");
+        }
+        public static String Output(int x) {
+            String str= depart[x].sperater() + depart[x].header();
+            foreach (Person g in Departmembers[x]) {
+                str += g.ToString(ref depart[x].Frist);
+            }
+            str += depart[x].ending();
+            return str;
+       }
+        public static String alloutput(){
+            String x="";
+            for (int y = 0; y < Departmembers.Length; y++)
+                x += Output(y);
+            return x;
+        }
+    }
+    class Department
+    {
+        String name; 
+        public Boolean Frist;
+        public Department(String str) { name = str; }
+        
+        public String sperater() { return "[separator headline=\"h2\" title=\""+name+"\"]\n\n[custom_table style=\"3\"]\n";
+        }
+        public String header()
+        {
+            return "\n<table width=\"100%\">\n<thead>\n<tr>\n<th width=\"20%\">Name</th>\n<th width=\"60%\">Title</th>\n<th width=\"20%\">Email</th></tr></thead><tbody>\n";
+        }
+        public String ending()
+        {
+            return "\n</tbody>\n</table>\n[/custom_table]\n";
+        }
+    }
+    class Person 
+    {   
+       public String Fname,Lname,email,department, role;
+       
+        String name() {
+            return Fname + " " + Lname;
+        }
+       public String ToString(ref bool Frist) {
+           String str = "\n";
+           if (!Frist) {
+               str += "<tr>\n";
+               str += "<td width=\"20%\">" + name() + "</td>\n";
+               str += "<td width=\"60%\">" + role + "</td>\n";
+               //str += "<td width=\"20%\"><a href=\"mailto:" + email + "\">zmehta@purdue.edu</a></td>\n";
+                str+= "<td width=\"20%\"><span data-sheets-value=\"[null,2,&quot;"+email+"&quot;]\" data-sheets-userformat=\"[null,null,16899,[null,0],[null,2,65535],null,null,null,null,null,null,null,0,null,null,null,null,1]\"><a href=\""+email+"\">"+email+"</a></span></td>";
+               str += "<tr>\n";
+               Frist = true;
+           }
+           else
+           {
+               
+               str += "<tr>\n";
+               str += "<td width=\"20%\">" + name() + "</td>\n";
+               str += "<td width=\"60%\">" + role + "</td>\n";
+               str += "<td width=\"20%\"><a href=\"mailto:" + email + "\">"+email+"</a></td>\n";
+               str += "<tr>\n";
+           }
+            return str;
+        }
+    }
+    class IO {
+        public static void load()
+        {
+            Roll.allmembers = new List<Person>();
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader("BOD.csv");
+            char[] separatingChars = { ',', '|' };
+            while ((line = file.ReadLine()) != null)
+            {
+                try
+                {
+                    List<string> x = new List<String>();
+                    String[] stuff = line.Split(separatingChars, StringSplitOptions.RemoveEmptyEntries);
+                    Person p = new Person();
+                    p.Fname = stuff[0];
+                    p.Lname = stuff[1];
+                    p.email = stuff[2];
+                    p.department = stuff[3];
+                    p.role = stuff[4];
+                    Roll.allmembers.Add(p);
+                    counter++;
+                }
+                catch (Exception e) { Console.Out.Write(e); }
+            }
+
+            file.Close();
+
+
+
+        }
+        public static void write(String x){
+
+            StreamWriter writetext = new StreamWriter("write.html");
+            writetext.WriteLine(x);
+            writetext.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Out.Write("Frist Start");
+            Roll.start();
+            IO.load();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Roll.gensList();
+            String outs = Roll.alloutput();
+            textBox1.Text = outs;
+            Console.Out.Write(outs);
+            IO.write(outs);
+        }
+    }
+}
